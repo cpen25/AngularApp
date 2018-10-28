f618bb303bab37714b1a7c43ede429ab93dcf1c2
PC <PC@PCs-MacBook-Pro.local>
2018-10-28 13:40:42 +1100

    commit a minor exercise

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
